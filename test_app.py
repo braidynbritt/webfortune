faaa2a6472f70348fcd2492d1fc84bfd3a6de44e
Braidyn Britt <braidynrbritt@gmail.com>
2023-04-25 13:52:24 -0500

    fixed test...again

--- test_app.py
+++ test_app.py
@@ -2,12 +2,12 @@ import pytest
 from appserver import app as flask_app
 
 @pytest.fixture
-    def app():
-        yield flask_app
+def app():
+    yield flask_app
 
 @pytest.fixture
-    def client(app):
-        return app.test_client()
+def client(app):
+    return app.test_client()
 
 def test_cowsay(app, client):
     message = 'hello'
